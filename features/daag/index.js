48032d6aacebe4b65a887354149bed3c39cbf837
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-10-09 17:33:22 -0400

    Update the daag feature to Get (or create) phases -> create scheme -> create scope -> create epics -> create workitems -> create changesets

--- features/daag/index.js
+++ features/daag/index.js
@@ -2,26 +2,85 @@ const Runner = require('./../../src/runner');
 const dropMoment = require('./../utils/dropMoment');
 const times = require('./../utils/times');
 
+const getScheme = (v1, phaseOids) => v1.create('Scheme', {
+    Name: 'ValveScheme',
+    SelectedValues: phaseOids
+});
+
+const getScope = (v1, schemeOid) => v1.create('Scope', {
+    Name: 'ValveScope1000',
+    Parent: 'Scope:0',
+    Scheme: schemeOid,
+    BeginDate: '2016-06-28'
+});
+
+const getPhase = (v1, name) => v1
+    .query({
+        from: 'Phase',
+        select: ['Name'],
+        where: {
+            Name: name
+        }
+    }).then(results => {
+        if(results[0][0]) {
+            return results[0][0]._oid
+        } else {
+            return v1.create('Phase', { Name: name }).then(phase => dropMoment(phase.id))
+        }
+    });
+
+const getEpics = (v1, scopeOid) => times(10).map(i => v1.create('Epic', {
+    Name: `ValveEpic${i}`,
+    Scope: scopeOid
+}));
+
+const getWorkitems = (v1, scopeOid, epics) => Promise.all(
+    epics
+        .reduce((promises, epic) => promises.concat(
+            times(10).map(i => v1.create((i % 2 === 0 ? 'Story' : 'Defect'), {
+                Name: `ValveStory${i}`,
+                Scope: scopeOid,
+                Super: dropMoment(epic.id)
+            }))
+        ), [])
+);
+
+const getChangeSets = (v1, workitems) => Promise.all(
+    workitems.map(workitem => {
+        const workitemOid = dropMoment(workitem.id);
+        return v1.create('ChangeSet', {
+            Name: 'ChangeSet',
+            PrimaryWorkitems: [
+                workitemOid
+            ]
+        })
+    })
+);
+
+
 module.exports = class Daag extends Runner {
     command() {
-        return new Promise((resolve, reject) => {
-            const v1 = this.authenticateAs('admin', 'admin');
+        const v1 = this.authenticateAs('admin', 'admin');
 
-            const promises = times(10).map(i => v1.create('Epic', {
-                Name: `ValveEpic${i}`,
-                Scope: 'Scope:0'
-            }));
+        const phases = [
+            getPhase(v1, 'Testing'),
+            getPhase(v1, 'Development'),
+            getPhase(v1, 'Production')
+        ];
 
-            const result = Promise.all(promises).then(epics => Promise.all(
-                epics.reduce((wiPromises, epic) => wiPromises
-                    .concat(times(4).map(i => v1.create((i % 2 === 0 ? 'Story' : 'Defect'), {
-                        Name: `ValveStory${i}`,
-                        Scope: 'Scope:0',
-                        Super: dropMoment(epic.id)
-                    }))), [])
-            ));
-
-            resolve(result);
-        });
+        return Promise.all(phases)
+            .then(phaseOids => {
+                return getScheme(v1, phaseOids)
+                    .then(scheme => {
+                        const schemeOid = dropMoment(scheme.id);
+                        return getScope(v1, schemeOid)
+                            .then(scope => {
+                                const scopeOid = dropMoment(scope.id);
+                                return Promise.all(getEpics(v1, scopeOid))
+                                    .then(epics => getWorkitems(v1, scopeOid, epics))
+                                    .then(workitems => getChangeSets(v1, workitems));
+                            });
+                    });
+            });
     }
 };
