e22e5e1a47fce40cf7f6aa08ec517664b2900575
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-10-12 17:45:20 -0400

    default task sets V1Host to localhost

--- tasks/default.js
+++ tasks/default.js
@@ -5,7 +5,7 @@ const path = require('path');
 gulp.task('default', [], function() {
     const contents = `V1Protocol=http
 V1Port=80
-V1Host=wsmith3
+V1Host=localhost
 V1Instance=VersionOne.Web
 V1Username=admin
 V1Password=admin`;
