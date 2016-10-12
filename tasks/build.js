d6c2b55decf032faf656ac567573a60f60e01b84
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-10-12 17:43:24 -0400

    pull babel config in to build task from package.json

--- tasks/build.js
+++ tasks/build.js
@@ -1,20 +1,9 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const config = require('../package.json');
 
 gulp.task('build', [], function() {
     return gulp.src('src/**/*.js')
-        .pipe(babel({
-            presets: [
-                'stage-3',
-                'stage-2',
-                'stage-1',
-                'stage-0',
-                'es2015'
-            ],
-            plugins: [
-                'transform-class-properties',
-                'transform-runtime'
-            ]
-        }))
+        .pipe(babel(config.babel))
         .pipe(gulp.dest('dist'))
 });
