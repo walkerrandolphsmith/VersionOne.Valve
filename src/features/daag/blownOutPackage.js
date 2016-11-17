b2f567f5f5800cebe8fb1d50d4e44aa60a8dbc81
walker smith <walker.smith@versionone.com>
2016-11-17 11:20:44 -0500

    Create two rows of bundles such that a bundle in the first row has significantly more workitems than any other bundle.

--- src/features/daag/blownOutPackage.js
+++ src/features/daag/blownOutPackage.js
@@ -41,111 +41,34 @@ module.exports = class ValveRunner extends Runner {
 
         const scopeOid = await getScope(v1, SCOPE_NAME, schemeOid);
 
-        const doneStories = await Promise.all(times(10).map(i => createDoneStory(v1, scopeOid)));
+        const doneStories = await Promise.all(times(5).map(i => createDoneStory(v1, scopeOid)));
         const doneChangeSets = await Promise.all(
             doneStories.map(story => createChangeSet(v1, [dropMoment(story.id)]))
         ).then(changeSets => changeSets.map(changeSet => dropMoment(changeSet.id)));
 
-        const onGoingStories = await Promise.all(times(10).map(i => createStory(v1, scopeOid)));
-        const onGoingChangeSets = await Promise.all(
-            onGoingStories.map(story => createChangeSet(v1, [dropMoment(story.id)]))
+        const manyStories = await Promise.all(times(20).map(i => createStory(v1, scopeOid)));
+        const manyChangeSets = await Promise.all(
+            manyStories.map(story => createChangeSet(v1, [dropMoment(story.id)]))
         ).then(changeSets => changeSets.map(changeSet => dropMoment(changeSet.id)));
 
         /*--------------------------------------------------------------------------------------------*/
-        /*----------------------------------- FULLY MATURED BUNDLE -----------------------------------*/
+        /*--------------------------- TWO ROWS WITH BLOWN OUT PACKAGE --------------------------------*/
         /*--------------------------------------------------------------------------------------------*/
         console.log('FULLY MATURED BUNDLE');
 
         const MATURED_BUNDLE_PACKAGE = 'Matured Bundle Package';
 
-        await createBundle(v1, developmentPhase, MATURED_BUNDLE_PACKAGE, [
-            doneChangeSets[0], doneChangeSets[1], doneChangeSets[2], doneChangeSets[3]
-        ]);
+        await createBundle(v1, developmentPhase, MATURED_BUNDLE_PACKAGE, manyChangeSets);
 
-        await createBundle(v1, testingPhase, MATURED_BUNDLE_PACKAGE, [
-            doneChangeSets[0], doneChangeSets[1], doneChangeSets[2], doneChangeSets[3]
-        ]);
+        await createBundle(v1, testingPhase, MATURED_BUNDLE_PACKAGE, doneChangeSets);
 
-        await createBundle(v1, productionPhase, MATURED_BUNDLE_PACKAGE, [
-            doneChangeSets[0], doneChangeSets[1], doneChangeSets[2], doneChangeSets[3]
-        ]);
+        await createBundle(v1, productionPhase, MATURED_BUNDLE_PACKAGE, doneChangeSets);
 
-        /*--------------------------------------------------------------------------------------------*/
-        /*--------------------------- MIXED WORKITEM STATUS BUNDLE -----------------------------------*/
-        /*--------------------------------------------------------------------------------------------*/
-        console.log('MIXED WORKITEM STATUS BUNDLE');
+        await createBundle(v1, developmentPhase, MATURED_BUNDLE_PACKAGE, doneChangeSets);
 
-        const MIXED_WI_STATUS_PACKAGE = 'Matured Bundle Package';
+        await createBundle(v1, testingPhase, MATURED_BUNDLE_PACKAGE, doneChangeSets);
 
-        await createBundle(v1, developmentPhase, MIXED_WI_STATUS_PACKAGE, [
-            ...doneChangeSets, onGoingChangeSets[0], onGoingChangeSets[1]
-        ]);
-
-
-        /*--------------------------------------------------------------------------------------------*/
-        /*------------------------------------ ROUGE PACKAGE -----------------------------------------*/
-        /*--------------------------------------------------------------------------------------------*/
-        console.log('ROUGE PACKAGE');
-
-        const ROUGE_PACKAGE = 'Rouge Package';
-
-        const rougeStories = await Promise.all(times(10).map(i => createDoneStory(v1, scopeOid)));
-        const rougeChangeSets = await Promise.all(
-            rougeStories.map(story => v1.create('ChangeSet', {Name: 'ChangeSet'}))
-        ).then(changeSets => changeSets.map(changeSet => dropMoment(changeSet.id)));
-
-        await createBundle(v1, developmentPhase, ROUGE_PACKAGE, [
-            rougeChangeSets[0], rougeChangeSets[1], rougeChangeSets[3], onGoingChangeSets[0]
-        ]);
-
-        await createBundle(v1, testingPhase, ROUGE_PACKAGE, [
-            rougeChangeSets[0], rougeChangeSets[1], rougeChangeSets[3], onGoingChangeSets[0], onGoingChangeSets[1]
-        ]);
-
-        await createBundle(v1, productionPhase, ROUGE_PACKAGE, [
-            rougeChangeSets[3], rougeChangeSets[4], onGoingChangeSets[3]
-        ]);
-
-        /*--------------------------------------------------------------------------------------------*/
-        /*----------------------------------- SHARED COMMIT BUNDLE -----------------------------------*/
-        /*--------------------------------------------------------------------------------------------*/
-        console.log('SHARED COMMIT BUNDLE');
-
-        const SHARED_COMMIT_PACKAGE = 'Shared Commit Package';
-
-        const sharedWorkitems = await Promise.all(
-            times(3).map(i => createStory(v1, scopeOid))
-        ).then(workitems => workitems.map(workitem => dropMoment(workitem.id)));
-        const sharedChangeSet = await createChangeSet(v1, sharedWorkitems)
-            .then(changeSet => dropMoment(changeSet.id));
-
-        await createBundle(v1, developmentPhase, SHARED_COMMIT_PACKAGE, [
-            sharedChangeSet
-        ]);
-
-        /*--------------------------------------------------------------------------------------------*/
-        /*----------------------------------- SPREAD WORKITEM BUNDLE ---------------------------------*/
-        /*--------------------------------------------------------------------------------------------*/
-        console.log('SPREAD WORKITEM BUNDLE');
-
-        const SPREAD__PACKAGE = 'Spread workitem in package';
-
-        const spreadWorkitem = await createStory(v1, scopeOid);
-        const spreadChangeSets = await Promise.all(
-            times(6).map(i => createChangeSet(v1, [dropMoment(spreadWorkitem.id)]))
-        ).then(changeSets => changeSets.map(changeSet => dropMoment(changeSet.id)));
-
-        await createBundle(v1, developmentPhase, SPREAD__PACKAGE, [
-            spreadChangeSets[0], spreadChangeSets[1]
-        ]);
-
-        await createBundle(v1, testingPhase, SPREAD__PACKAGE, [
-            spreadChangeSets[2], spreadChangeSets[3]
-        ]);
-
-        await createBundle(v1, productionPhase, SPREAD__PACKAGE, [
-            spreadChangeSets[4], spreadChangeSets[5]
-        ]);
+        await createBundle(v1, productionPhase, MATURED_BUNDLE_PACKAGE, doneChangeSets);
 
         return Promise.resolve();
     }
