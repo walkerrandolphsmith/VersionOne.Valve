b1ed17ab68bae4bdcb8509d94b48e901900d29cb
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-11-21 18:55:47 -0500

    Use the correct url for production docs.

--- gulpfile.js
+++ gulpfile.js
@@ -43,7 +43,7 @@ const config = {
         dest: `${dest}/css`
     },
     url: process.env.NODE_ENV === 'production'
-        ? 'http://www.github.com/walkerrandolphsmith/VersionOne.Valve' : 'http://localhost:3000'
+        ? 'http://walkerrandolphsmith.github.io/VersionOne.Valve' : 'http://localhost:3000'
 };
 
 const templatePath = `${src}/templates`;
