d4b3e40978b52c2dadf0b2670a8aebf136a01d5a
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-11-26 17:39:33 -0500

    Use throttle when creating lots of epics

--- src/features/daag/loe.js
+++ src/features/daag/loe.js
@@ -5,7 +5,8 @@ import throttler from './../../common/throttler';
 import {
     getDaagScope
     createStory,
-    createChangeSet
+    createChangeSet,
+    createSpreadWorkitem
 } from './utils';
 
 const SCOPE_NAME = 'ValveScope 100 for one 10 for the rest';
@@ -14,47 +15,37 @@ module.exports = class ValveRunner extends Runner {
     async command() {
         const v1 = this.authenticateAs('admin', 'admin');
 
-        const scopeOid = await getDaagScope(v1, SCOPE_NAME);
+        const {
+            scopeOid, epicCategory,
+            developmentPhase, testingPhase, productionPhase
+        } = await getDaagScope(v1, SCOPE_NAME);
 
         /*--------------------------------------------------------------------------------------------*/
         /*---------------------------------- Create tons of epics ------------------------------------*/
         /*--------------------------------------------------------------------------------------------*/
-        
-        
-        const epicPromises = times(300).map(i => () => v1.create('Epic', {
-            Name: 'ValveEpic LoadEpic ' + i,
-            Category: epicCategory,
-            Scope: scopeOid
-        }));
-        
-        const epics = await throttler(epicPromises, 25);
-        
-        epics.map(epic => {
-            
-        })
+
+
+        const epicPromises = times(1000).map((epicCount) => {
+            return () => v1.create('Epic', {
+                Name: 'ValveEpic LoadEpic ' + epicCount,
+                Category: epicCategory,
+                Scope: scopeOid
+            })
+        });
+        let loeEpics = await throttler(epicPromises, 25);
 
         console.log('creating stories on epics');
-        var once = false; 
-        while (loeEpics.length > 0) {
-            console.log("epics remaining: ", loeEpics.length);
-            await Promise.all(loeEpics.slice(0, 2).map(async(epic) => {
-            const numberOfItems = once ? 10 : 100;
-        once = true;
-                const loeStories = await Promise.all(times(numberOfItems).map(i => createStory(v1, scopeOid)));
-                const loeChangeSets = await Promise.all(
-                    loeStories.map(story => createChangeSet(v1, [dropMoment(story.id)]))
-                ).then(changeSets => changeSets.map(changeSet => dropMoment(changeSet.id)));
 
-                return Promise.all(
-                    loeStories.map(story => v1.update(dropMoment(story.id), {
+        const storyPromises = loeEpics.map((epic)=>{
+            return () => Promise.all(times(10).map(async (i)=> {
+                    const swi = await createSpreadWorkitem(v1, scopeOid, developmentPhase, testingPhase, productionPhase);
+                    return v1.update(swi, {
                         Super: dropMoment(epic.id)
-                    }))
-                )
-            }));
-
-            loeEpics.splice(0, 2);
-        }
-
+                    })
+                }
+            ))
+        });
+        await throttler(storyPromises, 25);
         return Promise.resolve();
     }
 };
