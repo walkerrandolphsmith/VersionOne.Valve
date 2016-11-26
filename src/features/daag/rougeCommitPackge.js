845ac192ef3cdf8dff7e24a29e8fd68075ed490a
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-11-26 17:51:47 -0500

    Rogue package only creates rouge commits and asscoiates them to a bundle.

--- src/features/daag/rougeCommitPackge.js
+++ src/features/daag/rougeCommitPackge.js
@@ -1,11 +1,7 @@
 import Runner from './../../runner';
 import dropMoment from './../../common/dropMoment';
 import times from './../../common/times';
-import {
-    getDaagScope
-    createDoneStory,
-    createBundle
-} from './utils';
+import { getDaagScope, createBundle } from './utils';
 
 const SCOPE_NAME = 'ValveScope';
 
@@ -14,7 +10,7 @@ module.exports = class ValveRunner extends Runner {
     async command() {
         const v1 = this.authenticateAs('admin', 'admin');
 
-        const { scopeOid, developmentPhase, testingPhase, productionPhase } = await getDaagScope(v1, SCOPE_NAME);
+        const { developmentPhase } = await getDaagScope(v1, SCOPE_NAME);
 
         /*--------------------------------------------------------------------------------------------*/
         /*------------------------------------ ROUGE PACKAGE -----------------------------------------*/
@@ -23,28 +19,11 @@ module.exports = class ValveRunner extends Runner {
 
         const ROUGE_PACKAGE = 'Rouge Package';
 
-        const rougeStories = await Promise.all(times(10).map(i => createDoneStory(v1, scopeOid)));
         const rougeChangeSets = await Promise.all(
-            rougeStories.map(story => v1.create('ChangeSet', {Name: 'ChangeSet'}))
+            times(10).map(i => v1.create('ChangeSet', {Name: 'ChangeSet'}))
         ).then(changeSets => changeSets.map(changeSet => dropMoment(changeSet.id)));
 
-
-        const onGoingStories = await Promise.all(times(10).map(i => createDoneStory(v1, scopeOid)));
-        const onGoingChangeSets = await Promise.all(
-            onGoingStories.map(story => v1.create('ChangeSet', {Name: 'ChangeSet'}))
-        ).then(changeSets => changeSets.map(changeSet => dropMoment(changeSet.id)));
-
-        await createBundle(v1, developmentPhase, ROUGE_PACKAGE, [
-            rougeChangeSets[0], rougeChangeSets[1], rougeChangeSets[3], onGoingChangeSets[0]
-        ]);
-
-        await createBundle(v1, testingPhase, ROUGE_PACKAGE, [
-            rougeChangeSets[0], rougeChangeSets[1], rougeChangeSets[3], onGoingChangeSets[0], onGoingChangeSets[1]
-        ]);
-
-        await createBundle(v1, productionPhase, ROUGE_PACKAGE, [
-            rougeChangeSets[3], rougeChangeSets[4], onGoingChangeSets[3]
-        ]);
+        await createBundle(v1, developmentPhase, ROUGE_PACKAGE, rougeChangeSets);
 
         return Promise.resolve();
     }
